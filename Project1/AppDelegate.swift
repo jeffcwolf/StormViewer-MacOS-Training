e84319f29b9f965168a7b3772ee170552d82b240
Jeff Wolf <jeffcwolf@gmail.com>
2020-08-30 20:21:21 +0200

    Initial Commit

---
+++ Project1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project1
+//
+//  Created by Jeffrey Wolf on 30.08.20.
+//  Copyright © 2020 Jeffrey Wolf. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project1/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Jeffrey Wolf on 30.08.20.
+//  Copyright © 2020 Jeffrey Wolf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
